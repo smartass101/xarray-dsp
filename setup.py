41143f57503cb8bffb0d84773f851c31fad413b2
Ondrej Grover <ondrej.grover@gmail.com>
2018-08-08 14:43:41 +0200

    release 0.2, bump xarray dep to 0.9 since we use to_coord

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='xarray-compatible digital signal processing tools',
-    version='0.1',
+    name='xarray-dsp',
+    description='xarray-compatible digital signal processing tools',
+    version='0.2',
     packages=['xarray_dsp'],
     requires=[
         'numpy',
         'scipy',
-        'xarray'
+        'xarray>=0.9.0'
     ],
     author='Ondrej Grover',
     author_email='ondrej.grover@gmail.com',
